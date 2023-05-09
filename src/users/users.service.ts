53a5ea91a88ce911233bf0349a55e2aa9ea89081
Muhammad Mukhtiar <mukhtiarfsd@gmail.com>
2023-05-09 13:40:17 +0000

    changee

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { User } from 'src/users/models/user.model';
+import { User } from './models/user.model';
 
 @Injectable()
 export class UsersService {
